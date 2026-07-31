8f22d8691620a981fa384bc987846134d890282a
Michael <mikexbear@gmail.com>
2026-08-01 00:31:17 +0700

    chore: initial commit

---
+++ src/main/java/com/github/theyolkyproject/yolkycore/YolkyCoreApplication.java
@@ -0,0 +1,11 @@
+package com.github.theyolkyproject.yolkycore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YolkyCoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YolkyCoreApplication.class, args);
+    }
+}
---
+++ src/test/java/com/github/theyolkyproject/yolkycore/YolkyCoreApplicationTests.java
@@ -0,0 +1,10 @@
+package com.github.theyolkyproject.yolkycore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YolkyCoreApplicationTests {
+    @Test
+    void contextLoads() { }
+}
